94389bc07b0341756a20735e0b06c8355ef52bb4
TAHiRLI <tahrli.tahirr@gmail.com>
2022-12-23 21:36:51 +0400

    houses index page ready

---
+++ QuarterProject/Quarter/Quarter/Areas/Admin/Controllers/HouseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Quarter.DAL;
+
+namespace Quarter.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HouseController : Controller
+    {
+        private readonly QuarterDbContext _context;
+
+        public HouseController(QuarterDbContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index()
+        {
+            var model = _context.Houses
+                .Include(x=> x.Category)
+                .Include(x=> x.Owner)
+                .Include(x=> x.City)
+                .Include(x=> x.HouseImages)
+                .ToList();
+            return View(model);
+        }
+    }
+}
