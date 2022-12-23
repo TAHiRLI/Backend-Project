5e645bb103a6d2848801e44832b6d5d726ab5b4c
TAHiRLI <tahrli.tahirr@gmail.com>
2022-12-23 21:50:32 +0400

    services read done

---
+++ QuarterProject/Quarter/Quarter/Areas/Admin/Controllers/ServiceController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Quarter.DAL;
+
+namespace Quarter.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServiceController : Controller
+    {
+        private readonly QuarterDbContext _context;
+
+        public ServiceController(QuarterDbContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index()
+        {
+            var model = _context.Services.ToList();
+            return View(model);
+        }
+    }
+}
