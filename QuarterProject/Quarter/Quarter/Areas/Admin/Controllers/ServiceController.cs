2116bbdf4f26895e6fde6b46c447a0066fdd7829
TAHiRLI <tahrli.tahirr@gmail.com>
2022-12-23 22:13:31 +0400

    service edit done

--- QuarterProject/Quarter/Quarter/Areas/Admin/Controllers/ServiceController.cs
+++ QuarterProject/Quarter/Quarter/Areas/Admin/Controllers/ServiceController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.Internal;
 using Quarter.DAL;
+using Quarter.Models;
 
 namespace Quarter.Areas.Admin.Controllers
 {
@@ -16,6 +18,56 @@ namespace Quarter.Areas.Admin.Controllers
         {
             var model = _context.Services.ToList();
             return View(model);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Service service)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _context.Services.Add(service);
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var service = _context.Services.FirstOrDefault(x => x.Id == id);
+            if (service == null)
+                return NotFound();
+            return View(service);
+        }
+        [HttpPost]
+        public IActionResult Edit(Service service)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var existService = _context.Services.FirstOrDefault(x => x.Id == service.Id);
+            if (existService == null)
+                return NotFound();
+            existService.Name = service.Name;
+            existService.Desc = service.Desc;
+            existService.IsFeatured = service.IsFeatured;
+            existService.Icon = service.Icon;
+            _context.SaveChanges();
+
+
+            return RedirectToAction("index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            
+            return Ok();
         }
     }
 }
