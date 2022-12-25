fb5fa130096a568ae41f597f060318faace03d52
TAHiRLI <tahrli.tahirr@gmail.com>
2022-12-25 12:12:30 +0400

    details page done partialy

--- QuarterProject/Quarter/Quarter/Controllers/HouseController.cs
+++ QuarterProject/Quarter/Quarter/Controllers/HouseController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query.Internal;
 using Quarter.DAL;
 using Quarter.Models;
+using Quarter.ViewModels;
 
 namespace Quarter.Controllers
 {
@@ -25,6 +26,32 @@ namespace Quarter.Controllers
                 .Include(x=>x.HouseImages)
                 .FirstOrDefault(x => x.Id == id);
             return PartialView("_HouseModalPartial", house);
+        }
+        public IActionResult Details(int id )
+        {
+            var house = _context.Houses
+                .Include(x=> x.HouseImages)
+                .Include(x=> x.HouseAmenities).ThenInclude(x=> x.Amenity)
+                .Include(x=> x.Owner)
+                .Include(x=> x.City)
+                .Include(x=> x.Category)
+                .FirstOrDefault(x => x.Id == id);
+
+            if (house == null)
+                return NotFound();
+
+
+            DetailsViewModel DetailsVm = new DetailsViewModel();
+            DetailsVm.House = house;
+            DetailsVm.RelatedProducts = _context.Houses
+                .Include(x=> x.City)
+                .Include(x=> x.Category)
+                .Include(x=> x.HouseImages)
+                .Where(x => x.OwnerId == house.OwnerId && x.Id != house.Id).ToList();
+
+
+            DetailsVm.TopCategories = _context.Categories.Include(x=> x.Houses).OrderByDescending(x=> x.Houses.Count).Take(5).ToList();
+            return View(DetailsVm);
         }
     }
 }
---
+++ QuarterProject/Quarter/Quarter/ViewModels/DetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Quarter.Models;
+
+namespace Quarter.ViewModels
+{
+    public class DetailsViewModel
+    {
+        public House House { get; set; }
+        public List<House> RelatedProducts { get; set; }
+        public List<Category> TopCategories { get; set; }
+    }
+}
